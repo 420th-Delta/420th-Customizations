401cf279c8a6ed99948a6d06ca8cc381de203d66
online1034 <iaydrwg200997@qq.com>
2026-07-06 09:16:45 +0800

    add mag support for MP5 and UMP from JCA IA

--- addons/magwells_jca/config.cpp
+++ addons/magwells_jca/config.cpp
@@ -49,6 +49,26 @@ class CfgMagazineWells {
             "20Rnd_762x51_Mag",
         };
     };
+    class JCA_MP5_9x19 {
+        fdelta_magwells_magazines[] = {
+            "30Rnd_9x21_Mag",
+            "30Rnd_9x21_Red_Mag",
+            "30Rnd_9x21_Yellow_Mag",
+            "30Rnd_9x21_Green_Mag",
+            "30Rnd_9x21_Mag_SMG_02",
+            "30Rnd_9x21_Mag_SMG_02_Tracer_Red",
+            "30Rnd_9x21_Mag_SMG_02_Tracer_Yellow",
+            "30Rnd_9x21_Mag_SMG_02_Tracer_Green",
+        };
+    };  
+    class JCA_UMP_45ACP {
+        fdelta_magwells_magazines[] = {
+            "30Rnd_45ACP_Mag_SMG_01",
+            "30Rnd_45ACP_Mag_SMG_01_tracer_green",
+            "30Rnd_45ACP_Mag_SMG_01_Tracer_Red",
+            "30Rnd_45ACP_Mag_SMG_01_Tracer_Yellow"
+        };
+    };         
     class JCA_AWM_338LM {
         fdelta_magwells_magazines[] = {
             "10Rnd_338_Mag",
