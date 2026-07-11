d5390c5fe22fce3c8b62e21ead0aef8ba1c8dc92
Seathre0420 <steven@420thdelta.net>
2026-07-11 17:44:30 -0400

    feat: rebalance SDB indirect damage

---
+++ addons/ammo/config.cpp
@@ -0,0 +1,23 @@
+class CfgPatches {
+    class fdelta_ammo {
+        name = "420th Customizations - Ammunition";
+        author = "Seathre0420";
+        url = "https://github.com/Seathre0420/420th-Customizations";
+
+        requiredVersion = 2.20;
+        requiredAddons[] = {
+            "A3_Weapons_F_Jets",
+        };
+        skipWhenMissingDependencies = 0;
+
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+class CfgAmmo {
+    class ammo_Bomb_SDB {
+        indirectHit = 200;
+        indirectHitRange = 5;
+    };
+};
