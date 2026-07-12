e69a8cb6ee41e221e8268f43b8a1fd3ba1a5c11e
Seathre0420 <166345186+Seathre0420@users.noreply.github.com>
2026-07-12 14:56:03 -0400

    feat: increase splash damage of SDBs (#1)

---
+++ addons/ammo/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches {
+    class fdelta_ammo {
+        name = "420th Customizations - Vanilla Ammunition Tweaks";
+        author = "Seathre0420";
+        url = "https://github.com/Seathre0420/420th-Customizations";
+
+        requiredVersion = 2.20;
+        requiredAddons[] = {
+            "A3_Weapons_F_Jets",
+            "fdelta_main",
+        };
+        skipWhenMissingDependencies = 0;
+
+        units[] = {};
+    };
+};
+
+class CfgAmmo {
+    class ammo_Bomb_SmallDiameterBase;
+    class ammo_Bomb_SDB : ammo_Bomb_SmallDiameterBase {
+        indirectHit = 200; // 85
+        indirectHitRange = 5; // 3
+    };
+};
